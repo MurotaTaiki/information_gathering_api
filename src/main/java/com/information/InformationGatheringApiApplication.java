eb0ab4fd3ea90a4f0e81d2eb62c097ef6f7cc8e8
Murota Taiki <mrttik0508@gmail.com>
2020-01-26 20:46:35 +0900

    プロジェクトの作成

---
+++ src/main/java/com/information/InformationGatheringApiApplication.java
@@ -0,0 +1,13 @@
+package com.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationGatheringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationGatheringApiApplication.class, args);
+	}
+
+}
